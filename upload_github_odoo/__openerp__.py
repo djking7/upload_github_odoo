9db637d8a775d36030128e8bd94d568903b728ce
king ramzi <foulenne2@gmail.com>
2017-01-20 16:21:50 +0100

    never mind

---
+++ upload_github_odoo/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Zip Install",
+    "summary": "Install zip file from github",
+    "description": "",
+    "author": "King",
+    "website": "github.com/djking7",
+    "application": True,
+    "installable": True,
+    "category": "",
+    "depends": ['base'],
+    "data":[
+    ]
+}
